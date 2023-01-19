a52b8a3ddb90b3fe6740fd8caa0145521808cd07
jeanettemontes <jeanettemontes@LEM231-12>
2023-01-18 18:59:21 -0800

    Add project files.

---
+++ COMP003.Assignment1/Program.cs
@@ -0,0 +1,18 @@
+﻿// this is a singl-line comment
+/*  This is a multi-line comment
+ *  Author Jeanette Montes
+ *  Course: COMP-003
+ *  Purpose: Lecture activity for this week
+ */
+namespace COMP003.Assignment1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!"); // add comments here too
+            Console.WriteLine("this is a string");
+            Console.WriteLine("Jeanette Montes");
+        }
+    }
+}
